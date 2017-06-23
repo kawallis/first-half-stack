2d1dbbc500c7c63ab2f2eafe4e7ed574f5e3bec3
Chris_Wallace <kawallis@icloud.com>
2017-06-23 16:31:20 -0700

    finished lab one and two

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-/* eslint no-console: "off" */
-
 const app = require('./lib/app');
 const http = require('http');
-const DB_URI = 'mongodb://localhost:27017/cinema'; 
-const PORT = 3000; 
+require('./lib/connect');
 
-const connection = require('./lib/connect');
-connection.connect(DB_URI);
-
+const PORT = process.env.PORT || 4000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log('Serving the fools on port de:', PORT);
+  console.log(`Server listening on ${PORT}`);
 });
--- test/server.test.js
+++ test/server.test.js
@@ -1,92 +1,46 @@
-const chai = require('chai');
-const assert = chai.assert;
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
 
-const app = require('../lib/app');
-const connection = require('../lib/connect');
+describe('Actor api', () => {
+  before(db.drop);
 
-const request = chai.request(app);
-describe('Unicorns REST API', () => {
+  let foonicorn = {
+    name: 'James Franco', 
+    color: 'black',
+    age: 33
+  };
 
-  const foonicorn = { name: 'brownie', color: 'brown' };
-  const DB_URI = 'mongodb://localhost:27017/unicorns-test';
-  
-  before(() => connection.connect(DB_URI));
-  before(() => connection.db.dropDatabase());
-  after(() => connection.close());
+  let id;
 
-  describe('GET /', () => {
-
-
-    it('says Checkout These Unicorns Foolio', () => {
-      return request
-        .get('/')
-        .then(res => (res.text))
-        .then(text => assert.equal(text, 'Checkout These Unicorns Foolio'));
-    });
+  it('initial /GET returns empty list', () => {
+    return request.get('/unicorns')
+            .then(req => {
+              const unicorns = req.body;
+              assert.deepEqual(unicorns, []);
+            });
   });
 
-  describe('POST /unicorns', () => {
-
-    it('saves the unicorn to the db', () => {
-      return request
-        .post('/unicorns')
-        .send(foonicorn)
-        .then(response => {
-          return response.body;
-        })
-        .then(savedUnicorn => {
-          assert.isOk(savedUnicorn._id);
-          foonicorn._id = savedUnicorn._id;
-          assert.deepEqual(savedUnicorn, foonicorn);
-        });
-    });
+  it('initial /post returns the posted object', () => {
+    return request.post('/unicorns')
+            .send(foonicorn)
+            .then(req => {
+              let unicorns = req.body;
+              id = req.body._id;
+              assert.deepEqual(unicorns.name, foonicorn.name);
+              assert.deepEqual(unicorns.age, foonicorn.age);
+              assert.deepEqual(unicorns.color, foonicorn.color);
+            });
   });
 
-  describe('GET /unicorns/:id', () => {
-
-    it('returns unicorn by id', () => {
-      return request
-        .get(`/unicorns/${foonicorn._id}`)
-        .then(res => {
-          return res.text;
-        })
-        .then(savedUnicorn => {
-
-          var savedacorn = JSON.parse(savedUnicorn);
-          assert.deepEqual(savedacorn, foonicorn);
-        });
-    });
+  it('initial /get/id get unicorn by id', () => {
+    return request.get(`/unicorns/${id}`)
+            .then(req => {
+              let unicorns = req.body;
+              
+              assert.deepEqual(unicorns.name, foonicorn.name);
+              assert.deepEqual(unicorns.age, foonicorn.age);
+              assert.deepEqual(unicorns.color, foonicorn.color);
+            });
   });
-
-  describe('PUT /unicorns/:nameToUpdate/:newName', () => {
-
-    it('update successful or not', () => {
-      return request
-        .put('/unicorns/brownie/blackie')
-        .then(res => {
-          return res.body;
-        })
-        .then(updatedUnicorn => {
-          var statusCode = updatedUnicorn.nModified;
-          assert.deepEqual(statusCode, 1);
-        });
-    });
-  }); 
-  describe('Delete /unicorns/:nameToDelete', () => {
-
-    it('delete successful or not', () => {
-      return request
-        .delete('/unicorns/blackie')
-        .then(res => {
-          return res.body;
-        })
-        .then(updatedUnicorn => {
-          var statusCode = updatedUnicorn.ok;
-          assert.deepEqual(statusCode, 1);
-        });
-    });
-  });
-
 });
